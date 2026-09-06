9c641b283d0febc0d15b934fe359442304cc9b20
Jupiter-NY <nyao.werty@gmail.com>
2026-09-06 12:39:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,7 @@
 window.TANK_CONFIG = {
   supabaseUrl: "https://iwugissyviygmlelemfs.supabase.co",
   supabasePublishableKey: "sb_publishable_nL9fg8pvDXnh9_Jlb9RjCA_nA1MCQlw",
+
+  multiplayerServer:
+    "wss://tiny-tank-maze.onrender.com/ws"
 };
